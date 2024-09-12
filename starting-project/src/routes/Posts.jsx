acf68055f0f9eff3b387c3991a3aac2cfaa2d18b
Namanjeet13 <namanjeetsingh@academic.rrc.ca>
2024-09-12 11:56:47 -0500

    React app

---
+++ starting-project/src/routes/Posts.jsx
@@ -0,0 +1,21 @@
+import { Outlet } from "react-router-dom";
+import PostsLists from "../components/PostsList";
+
+function Posts() {
+  return (
+    <>
+      <Outlet />
+      <main>
+        <PostsLists />
+      </main>
+    </>
+  );
+}
+
+export default Posts;
+
+export async function loader() {
+  const response = await fetch('http://localhost:8080/posts');
+  const resData = await response.json();
+  return resData.posts;
+}
